b5dfe9000e8d0b280b9fc9b0adf99c7e033fe552
Yangxh <2693307903@qq.com>
2019-11-21 21:04:00 +0800

    获取文章类别列表

--- web_back/js/category.js
+++ web_back/js/category.js
@@ -1,22 +1,22 @@
 var category = {
 
     get: function(callback) {
-        $.get(category_get, function(res) {
+        $.get(APIURLS.category_get, function(res) {
             callback(res);
         })
     },
     add: function(name, slug, callback) {
-        $.post(category_add, { 'name': name, 'slug': slug }, function(res) {
+        $.post(APIURLS.category_add, { 'name': name, 'slug': slug }, function(res) {
             callback(res);
         })
     },
     edit: function(id, name, slug, callback) {
-        $.post(category_edit, { 'id': id, 'name': name, 'slug': slug }, function(res) {
+        $.post(APIURLS.category_edit, { 'id': id, 'name': name, 'slug': slug }, function(res) {
             callback(res);
         })
     },
     del: function(callback) {
-        $.post(category_del, { 'id': id }, function(res) {
+        $.post(APIURLS.category_del, { 'id': id }, function(res) {
             callback(res);
         })
     }
